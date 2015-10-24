612806ce9dd5290387065f1da0a29fb6eb9230f8
Mike Moss <mrmoss@alaska.edu>
2015-10-24 11:42:14 -0800

    Added new firmware and old display pictures.

--- firmware/tester/tester.ino
+++ firmware/tester/tester.ino
@@ -1,7 +1,21 @@
 #include <Wire.h>
-#include <Adafruit_LEDBackpack.h>
-#include <Adafruit_GFX.h>
 #include <Adafruit_NeoPixel.h>
+
+#define ADAFRUIT_7SEGMENT
+//#define SPARKFUN_7SEGMENT
+
+#ifdef ADAFRUIT_7SEGMENT
+  #include <Adafruit_LEDBackpack.h>
+  #include <Adafruit_GFX.h>
+  typedef Adafruit_7segment display_t;
+  uint8_t display_i2c=0x70;
+#endif
+
+#ifdef SPARKFUN_7SEGMENT
+  #include "Sparkfun_LEDBackpack.h"
+  typedef Sparkfun_7segment display_t;
+  uint8_t display_i2c=0x71;
+#endif
 
 int xout_pin=A0;
 int yout_pin=A1;
@@ -11,7 +25,7 @@ int front_pin=10;
 int front_count=3;
 int dome_pin=12;
 int dome_count=1;
-Adafruit_7segment display;
+display_t display;
 Adafruit_NeoPixel front(front_count,front_pin,NEO_GRB+NEO_KHZ800);
 Adafruit_NeoPixel dome(dome_count,dome_pin,NEO_GRB+NEO_KHZ800);
 
@@ -24,7 +38,7 @@ void setup()
 
   front.begin();
   dome.begin();
-  display.begin(0x70);
+  display.begin(display_i2c);
 
   Serial.begin(57600);
 }
